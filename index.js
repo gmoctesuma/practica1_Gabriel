862c2b8b01cecab91be3878250229a7d32047694
gmoctesuma <89799218+gmoctesuma@users.noreply.github.com>
2021-09-06 11:50:57 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+fucion getIMC(){
+  var peso=parseFloat(document.getElementById("peso").value;
+  var altura=parseFloat(document.getElementById("altura").value;
+  var IMC=peso/(altura*altura);
+  document.getElementById("IMC").innerHTML=IMC;
+}
